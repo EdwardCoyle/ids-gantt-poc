118cb3fcd19f4d9e596cb10d7a9e15321a9b7f38
Ed Coyle <ed.coyle@infor.com>
2022-11-02 15:27:55 -0400

    Fix sizes/padding in cells

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,64 @@
+import type { IdsGanttChartData } from './data';
+import { data } from './data';
+
 const app = document.getElementById('gantt')!;
 const taskPane = app.querySelector<HTMLDivElement>('.gantt-task-pane')!;
 const timeline = app.querySelector<HTMLDivElement>('.gantt-timeline')!;
 
+// @TODO pull this from some sort of calendar configuration (days/months/etc)
 const gridCols = 30;
-const gridRows = 9;
+
+const getTotalRows = (thisData: IdsGanttChartData) => {
+    let rows = 0;
+    rows += thisData.groups.length;
+    thisData.groups.forEach((group: any) => {
+        rows += group.contents?.length || 0;
+    });
+    return rows;
+}
+
+const getTaskPaneColsCSS = () => {
+    let cols = '';
+    for (let i = 0; i < data.headers.length; i++) {
+        cols += ' minmax(100px, 1fr)';
+    }
+    return cols;
+};
+
+const renderTaskPaneColumns = (thisData: IdsGanttChartData) => {
+    const totalRows = getTotalRows(thisData);
+    const totalCells = totalRows * 2;
+    let html = '';
+
+    // Render Headers
+    html += '<div class="gantt-task-pane-header gantt-cell-container">';
+    for (let i = 0; i < thisData.headers.length; i++) {
+        html += `<div class="gantt-cell">${thisData.headers[i].name}</div>`;
+    }
+    html += '</div>';
+
+    // Render Cells
+    html += '<div class="gantt-task-pane-cells gantt-cell-container">';
+    for (let i = 0; i < totalCells; i++) {
+        html += '<div class="gantt-cell">&nbsp;</div>';
+    }
+    html += '</div>';
+
+    return html;
+}
 
 // Creates a CSS Grid Columns definition for the timeline based on data size
-const getGridColsCSS = () => {
+const getTimelineColsCSS = () => {
     let cols = '';
     for (let i = 0; i < gridCols; i++) {
-        cols += ' 1fr'; // Each date cell
+        cols += ' minmax(44px, 1fr)'; // Each date cell
     }
     return cols;
 };
 
 // Renders timeline grid columns
-const renderGridColumns = () => {
-    const totalCells = gridCols * gridRows;
+const renderTimelineColumns = (thisData: IdsGanttChartData) => {
+    const totalCells = gridCols * getTotalRows(thisData);
     let html = '';
 
     // Add header cells
@@ -38,9 +80,10 @@ const renderGridColumns = () => {
 
 document.addEventListener('DOMContentLoaded', () => {
     // Render task pane contents
-    // @TODO
+    taskPane.style.setProperty('--task-pane-grid-columns', getTaskPaneColsCSS());
+    taskPane.insertAdjacentHTML('afterbegin', renderTaskPaneColumns(data));
 
-    // Render grid columns
-    timeline.style.setProperty('--timeline-grid-columns', getGridColsCSS());
-    timeline.insertAdjacentHTML('afterbegin', renderGridColumns());
+    // Render timeline contents
+    timeline.style.setProperty('--timeline-grid-columns', getTimelineColsCSS());
+    timeline.insertAdjacentHTML('afterbegin', renderTimelineColumns(data));
 });
