4b53dcb89505045c30a3dd93637a7c2a093c3423
Ed Coyle <ed.coyle@infor.com>
2022-11-02 12:33:46 -0400

    Add some scaffold

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,12 @@
-// Code is here
+const app = document.getElementById('gantt')!;
+const timeline = app.querySelector('.gantt-timeline')!;
+
+const gridCols = 30;
+const gridRows = 9;
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Render task pane contents
+
+    // Render grid columns
+
+});
